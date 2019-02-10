2291ca08e394a33d50cf99ae457bc994114fba20
Solomon Bloch <solomonbloch@gmail.com>
2019-02-09 22:05:41 -0500

    start

--- spotify.lisp
+++ spotify.lisp
@@ -7,20 +7,23 @@
 
 
 (defcommand search-track (track) ((:string "track: "))
-  (let* ((results (lispotify:search-track track))
-         (results-alist (loop for i in results
-                              collecting
-                              `(,(concatenate 'string (jsown:val i "name") " - "
-                                              (jsown:val (jsown:val i "album") "name")
-                                              " - "
-                                              (format nil "~{~a~^ & ~}"
-                                                      (loop for artist in (jsown:val i "artists")
-                                                            collecting (jsown:val artist "name"))))
-                                 ,(jsown:val i "uri"))))
-         (choice (select-from-menu (current-screen)
-                                   results-alist nil 0 nil)))
-    (if (null choice)
-        nil
-        (lispotify:play-spotify-uri (second choice)))))
+  (if (null track)
+      (throw 'error "Abort.")
+      (let* ((results (lispotify:search-track track))
+             (results-alist (loop for i in results
+                                  collecting
+                                  `(,(concatenate 'string (jsown:val i "name") " - "
+                                                  (jsown:val (jsown:val i "album")
+                                                             "name")
+                                                  " - "
+                                                  (format nil "~{~a~^ & ~}"
+                                                          (loop for artist in (jsown:val i "artists")
+                                                                collecting (jsown:val artist "name"))))
+                                    ,(jsown:val i "uri"))))
+             (choice (select-from-menu (current-screen)
+                                       results-alist nil 0 nil)))
+        (if (null choice)
+            nil
+            (lispotify:play-spotify-uri (second choice))))))
 
 (define-key stumpwm:*top-map* (stumpwm:kbd "s-m") "search-track")
