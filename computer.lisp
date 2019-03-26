1fd5375e486f9d0c31bedc63ad9fe1e074e4fe9a
Solomon Bloch <solomonbloch@gmail.com>
2019-03-26 20:02:12 +0900

    Changing to a font that isn't TTF to fix crashing.

--- computer.lisp
+++ computer.lisp
@@ -4,3 +4,6 @@
 
 ;; (set-font (make-instance 'xft:font :family "TerminessTTF Nerd Font" :subfamily "Bold" :size 18))
 (set-font "-xos4-terminus-medium-r-normal--32-320-72-72-c-160-iso10646-1")
+
+
+;; (font-exists-p  "-xos4-terminus-medium-r-normal--32-320-72-72-c-160-iso10646-1")
