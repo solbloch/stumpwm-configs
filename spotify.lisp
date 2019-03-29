b3064a65f55b37b4f7cd1b17440d724aa20512c3
Solomon Bloch <solomonbloch@gmail.com>
2019-03-29 19:44:06 +0900

    trying to fix format (bad :()

--- spotify.lisp
+++ spotify.lisp
@@ -10,16 +10,17 @@
   (if (null track)
       (throw 'error "Abort.")
       (let* ((results (lispotify:search-track track))
-             (results-alist (loop for i in results
-                                  collecting
-                                  `(,(concatenate 'string (jsown:val i "name") " - "
-                                                  (jsown:val (jsown:val i "album")
-                                                             "name")
-                                                  " - "
-                                                  (format nil "~{~a~^ & ~}"
-                                                          (loop for artist in (jsown:val i "artists")
-                                                                collecting (jsown:val artist "name"))))
-                                    ,(jsown:val i "uri"))))
+             (results-alist
+               (loop for i in results
+                     collecting
+                     `(,(concatenate 'string (jsown:val i "name") " - "
+                                     (jsown:val (jsown:val i "album")
+                                                "name")
+                                     " - "
+                                     (format nil "~{~a~^ & ~}"
+                                             (loop for artist in (jsown:val i "artists")
+                                                   collecting (jsown:val artist "name"))))
+                       ,(jsown:val i "uri"))))
              (choice (select-from-menu (current-screen)
                                        results-alist nil 0 nil)))
         (if (null choice)
