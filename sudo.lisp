ea32a88d0d0368270963553bfec3250eacb2f2b1
Solomon Bloch <solomonbloch@gmail.com>
2019-03-31 01:52:43 +0900

    renamed my password file to sudo

---
+++ sudo.lisp
@@ -0,0 +1,30 @@
+(in-package :stumpwm)
+
+(setf *shell-program* "/bin/bash")
+
+(define-stumpwm-type :password (input prompt)
+  (or (argument-pop input)
+      (read-one-line (current-screen) prompt :password t)))
+
+(define-stumpwm-type :shell-su (input prompt)
+  (declare (ignore prompt))
+  (let ((prompt "$ "))
+    (or (argument-pop input)
+        (completing-read (current-screen) prompt 'complete-program))))
+
+(defun sudo (command &optional collect?)
+  (let* ((pass (read-one-line (current-screen) "sudo password: " :password t))
+         (command (concatenate 'string "echo " pass " | sudo -S " command)))
+    (run-shell-command command collect?)))
+
+(defcommand sudo-shell (command &optional collect?) ((:shell-su ""))
+  "no side effects, please (stump will break)"
+  (if (eq 0 (search "sudo" command))
+      (sudo command collect?)
+      (if collect?
+          (message "~a" (run-shell-command command t))
+          (run-shell-command command))))
+
+(defun sudo-terminate (pid)
+  (let ((password (read-one-line (current-screen) "sudo password: " :password t)))
+    (run-shell-command (concatenate 'string "echo " password " | sudo -S kill " pid))))
