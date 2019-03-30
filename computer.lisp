a0ce5a9b422ab0b77a05c58085cb6c81324215c4
Solomon Bloch <solomonbloch@gmail.com>
2019-03-31 01:54:21 +0900

    no real change

--- computer.lisp
+++ computer.lisp
@@ -4,6 +4,7 @@
 
 ;; (set-font (make-instance 'xft:font :family "TerminessTTF Nerd Font" :subfamily "Bold" :size 18))
 (set-font "-xos4-terminus-medium-r-normal--32-320-72-72-c-160-iso10646-1")
+;; (set-font "-gnu-unifont-medium-r-normal-sans-16-160-75-75-c-80-iso10646-1")
 
 
 ;; (font-exists-p  "-xos4-terminus-medium-r-normal--32-320-72-72-c-160-iso10646-1")
