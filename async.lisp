f7e1c0530d1fe8bb103cc0a49b173f7874b1db89
Solomon Bloch <solomonbloch@gmail.com>
2019-04-06 19:00:09 +0900

    added async-run!!

---
+++ async.lisp
@@ -0,0 +1,13 @@
+(in-package :stumpwm)
+
+(defparameter *mode-line-shell* (uiop:launch-program "bash" :input :stream :output :stream))
+
+(defun async-run (command)
+  (write-line command (uiop:process-info-input *mode-line-shell*))
+  (force-output (uiop:process-info-input *mode-line-shell*))
+  (let* ((output-string (read-line (uiop:process-info-output *mode-line-shell*)))
+         (stream (uiop:process-info-output *mode-line-shell*)))
+    (if (listen stream)
+        (loop while (listen stream)
+              do (setf output-string (concatenate 'string output-string (read-line stream)))))
+    output-string))
