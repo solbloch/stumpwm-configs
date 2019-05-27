d64cdceea1a43cd88201a368a89f11604272ffbb
Solomon Bloch <solomonbloch@gmail.com>
2019-05-27 20:18:32 +0900

    open books

---
+++ book.lisp
@@ -0,0 +1,16 @@
+(in-package :stumpwm)
+
+(defvar *book-directory* #P"~/GOOGLE/books/")
+
+(defun book-list () (uiop:directory-files *book-directory*))
+
+(defcommand open-book () ()
+  (let ((choice
+          (select-from-menu (current-screen)
+                            (loop for i in (book-list)
+                                  collecting
+                                  (list (nth 5 (cl-ppcre:split "/" (namestring i)))
+                                        (namestring i))) nil 0 nil)))
+    (if (null choice)
+        (throw 'error "Aborted.")
+        (run-shell-command (concatenate 'string "evince \"" (cadr choice) "\"")))))
