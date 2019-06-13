dd974a9577dd1aa760405729a1f443b67af5234e
Solomon Bloch <solomonbloch@gmail.com>
2019-06-13 10:19:41 +0900

    default framing for windows

---
+++ frame.lisp
@@ -0,0 +1,11 @@
+;; azrin
+(define-frame-preference "q-tip"
+  (0 t t :class "Emacs"))
+
+(define-frame-preference "jarobi"
+  (0 t t :class "Spotify")
+  (1 t t :class "Ripcord"))
+
+  ;; (define-frame-preference "Emacs"
+  ;;   (0 t t :class "Emacs"))
+  ;; (frame-number raise lock &key create restore dump-name class instance type role title)
