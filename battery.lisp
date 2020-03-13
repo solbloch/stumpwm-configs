2b91e699cde7d73d5c20c6e930dea47ebe1b1327
Solomon Bloch <solomonbloch@gmail.com>
2020-03-13 15:33:42 -0400

    Update battery for Thinkpad T480! (Double battery)!

--- battery.lisp
+++ battery.lisp
@@ -1,32 +1,45 @@
 (in-package :stumpwm)
 
-(defun slurp (file)
-  "slurp in a little file (one line file)"
-  (with-open-file (temp-stream file)
-    (read temp-stream)))
+(defvar battery-list '("/sys/class/power_supply/BAT0/uevent" "/sys/class/power_supply/BAT1/uevent"))
 
-(defun slurp-stream (file)
-  "slurp in a little file (one line file)"
-  (with-open-file (temp-stream file)
-    (read-line temp-stream)))
+(defun battery-alist (battery)
+  (with-open-file (stream battery)
+    (loop for line = (read-line stream nil)
+          while line
+          collect (let ((line-split (str:split #\= line)))
+                    (cons (car line-split) (cadr line-split))))))
 
-(defun battery-percent ()
-  (let ((charge-full (slurp "/sys/class/power_supply/BAT0/charge_full"))
-        (charge-now (slurp "/sys/class/power_supply/BAT0/charge_now")))
-    (float (* 100 (/ charge-now charge-full)))))
-
-(defun battery-remaining-discharging ()
-  (let ((charge-now (slurp "/sys/class/power_supply/BAT0/charge_now"))
-        (current-now (slurp "/sys/class/power_supply/BAT0/current_now")))
-    (floor (float (/ charge-now current-now)))))
-
-(defun battery-remaining-charging ()
-  (let ((charge-now (slurp "/sys/class/power_supply/BAT0/charge_now"))
-        (current-now (slurp "/sys/class/power_supply/BAT0/current_now")))
-    (floor (float (/ charge-now current-now)))))
+(defun cdr-assoc-string (item alist)
+  (cdr (assoc item alist :test #'string=)))
 
 (defun battery-string ()
-  (if (search "Disch" (slurp-stream "/sys/class/power_supply/BAT0/status"))
-      (multiple-value-bind (hours minutes) (battery-remaining-discharging)
-        (format nil "~a:~2,'0D-^7 ~a% ^n" hours (round (* 60 minutes)) (round (battery-percent))))
-      "off "))
+  (let* ((bat-list (mapcar #'battery-alist battery-list))
+         (perc-list '())
+         (energy 0)
+         (power 1)
+         (capacity 0)
+         (time-remaining 0)
+         (status-list '()))
+    (loop for bat in bat-list
+          do (progn
+               (incf energy (parse-integer
+                             (cdr-assoc-string "POWER_SUPPLY_ENERGY_NOW" bat)))
+               (incf power (parse-integer
+                            (cdr-assoc-string "POWER_SUPPLY_POWER_NOW" bat)))
+               (incf capacity (parse-integer
+                               (cdr-assoc-string "POWER_SUPPLY_ENERGY_FULL" bat)))
+               (setf status-list (nconc status-list
+                                        (list (cdr-assoc-string "POWER_SUPPLY_STATUS" bat))))
+               (setf perc-list (nconc perc-list
+                                      (list `(,(cdr-assoc-string "POWER_SUPPLY_NAME" bat)
+                                              ,(cdr-assoc-string "POWER_SUPPLY_CAPACITY" bat)))))))
+    (setf time-remaining
+          (if (= power 1)
+              0
+              (if (find "Charging" status-list :test #'string=)
+                  (/ (- capacity energy) power)
+                  (/ energy power))))
+    (format nil "~a:~2,'0D - ~{~{~a~^:~}%~^ ~}"
+            (floor time-remaining)
+            (round (* 60 (rem time-remaining 1.0)))
+            perc-list)))
