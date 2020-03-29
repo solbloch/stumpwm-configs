98a417c4ff26f378d9f53c22b804d33876f6eaaf
Solomon Bloch <solomonbloch@gmail.com>
2020-03-29 15:53:58 -0400

    Load new files.

--- init.lisp
+++ init.lisp
@@ -35,6 +35,8 @@
 ;; (restore-from-file "/home/sol/.stumpwm.d/frames")
 
 (mapcar #'load '("~/.stumpwm.d/async.lisp"
+                 "~/.stumpwm.d/last-command.lisp"
+                 "~/.stumpwm.d/process.lisp"
                  "~/.stumpwm.d/book.lisp"
                  "~/.stumpwm.d/config-sync.lisp"
                  "~/.stumpwm.d/token.lisp"
@@ -49,18 +51,18 @@
                  "~/.stumpwm.d/greek.lisp"
                  "~/.stumpwm.d/scratchpad.lisp"
                  "~/.stumpwm.d/openvpn.lisp"
-                 "~/.stumpwm.d/keymap.lisp"
                  "~/.stumpwm.d/solb.lisp"
                  "~/.stumpwm.d/battery.lisp"
                  "~/.stumpwm.d/mode-line.lisp"
                  "~/.stumpwm.d/gaps-fullscreen.lisp"
-                 ;; "~/.stumpwm.d/auto-tile.lisp"
                  "~/.stumpwm.d/ring.lisp"
                  "~/.stumpwm.d/clipboard-history.lisp"
-                 "~/.stumpwm.d/next.lisp"))
+                 "~/.stumpwm.d/next.lisp"
+                 "~/.stumpwm.d/echo.lisp"
+                 "~/.stumpwm.d/keymap.lisp"))
 
 
-(set-prefix-key (kbd "C-M-t"))
-(setf *mouse-focus-policy* :click)
+                 (set-prefix-key (kbd "C-M-t"))
+                 (setf *mouse-focus-policy* :click)
 
-;; :sloppy :click :ignore
+                 ;; :sloppy :click :ignore
