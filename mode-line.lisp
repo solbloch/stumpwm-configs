f81c81874079f01aa5119b347542fc653a4c3548
Solomon Bloch <solomonbloch@gmail.com>
2022-10-13 15:41:05 -0400

    Include humidity.

--- mode-line.lisp
+++ mode-line.lisp
@@ -72,9 +72,10 @@
 (defun weather-string ()
   (let ((weather-request *weather-info*))
     (if weather-request
-        (format nil "~1$°F ~1$°C ~a"
+        (format nil "~1$°F ~1$°C ~d% ~a"
                 (multi-val weather-request "main" "temp")
                 (* 5/9 (- (multi-val weather-request "main" "temp") 32.0))
+                (multi-val weather-request "main" "humidity")
                 (multi-val (car (multi-val weather-request "weather")) "description"))
         "weather unavailable")))
 
